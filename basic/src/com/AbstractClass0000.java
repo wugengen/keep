7148f26325401961d4f95c25cab2f4bcaa2ea82a
ASUS_WGG <1445409981@qq.com>
2020-06-19 20:39:51 +0800

    Default Changelist

--- basic/src/com/AbstractClass0000.java
+++ basic/src/com/AbstractClass0000.java
@@ -1,14 +1,24 @@
 package com;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * @program: keep_studying
- * @description: 测试类抽象类
+ * @description:
+ * 测试类抽象类，抽象类中可以有main方法，可以运行
+ * 可以没有抽象方法
  * @author: xxx
  * @create: 2020-04-13 23:34
  */
 public abstract class AbstractClass0000
+
 {
-    public void test0(){
-        
-    };
+
+    public abstract void c();
+
+
+    public static void main(String[] args) {
+
+    }
 }
